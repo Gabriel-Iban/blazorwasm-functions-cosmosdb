0a25fbb1f762c8128d97874d873b89c3463c564c
gabriel-iban <gabrielibancf@gmail.com>
2024-05-15 09:08:38 -0700

    Error. faltaba  en asiginaciones al principio

--- CreacionProyectos.ps1
+++ CreacionProyectos.ps1
@@ -5,9 +5,9 @@ $Env:UsuarioGitHub = Read-Host "Nombre de Usuario de GitHub"
 $Env:DirectorioDondeSeCrearanLosProyectos = Read-Host "Directorio con la ruta completa donde se crearán los proyectos"
 Set-Location $Env:DirectorioDondeSeCrearanLosProyectos
 
-$Env:NombreCliente=$NombreComun + "BlazorWasm"
-$Env:NombreApi=$NombreComun + "Api"
-$Env:NombreModel=$NombreComun + "Model"
+$Env:NombreCliente=$Env:NombreComun + "BlazorWasm"
+$Env:NombreApi=$Env:NombreComun + "Api"
+$Env:NombreModel=$Env:NombreComun + "Model"
 
 gh repo create $Env:NombreCliente --private
 gh repo create $Env:NombreApi --private
@@ -36,7 +36,9 @@ dotnet new gitignore
 dotnet new blazorwasm -o $Env:NombreCliente
 dotnet sln add $Env:NombreCliente
 
-git submodule add https://github.com/$Env:UsuarioGitHub/$Env:NombreModel.git
+mkdir $Env:NombreModel
+Copy-Item ..\$Env:NombreModel\* .\$Env:NombreModel -Recurse
+#git submodule add https://github.com/$Env:UsuarioGitHub/$Env:NombreModel.git $Env:NombreModel
 
 dotnet sln add $Env:NombreModel
 git add .
@@ -54,7 +56,7 @@ func init --worker-runtime dotnet-isolated --language c#-isolated
 func function new --name GetWeather --authlevel anonymous --template HttpTrigger
 Set-Location ..
 dotnet sln add $Env:NombreApi
-git submodule add https://github.com/$Env:UsuarioGitHub/$Env:NombreModel.git 
+git submodule add https://github.com/$Env:UsuarioGitHub/$Env:NombreModel.git $Env:NombreModel
 dotnet sln add $Env:NombreModel
 git add .
 git commit -m"Commit inicial"
