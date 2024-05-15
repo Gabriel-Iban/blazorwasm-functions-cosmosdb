9dfa4e8f385120a20d0fce1f305dde20f25cc436
Gabriel-Iban <gabriel.iban.cf@gmail.com>
2024-05-15 03:54:46 -0700

    Con variables de entorno para poder usarlas fuera del script

--- CreacionProyectos.ps1
+++ CreacionProyectos.ps1
@@ -1,28 +1,24 @@
 #creación del los proyectos
-#$NombreComun="ejemplo3"
-#$UsuarioGitHub ="Gabriel-Iban"
 
-if(( $null -eq $NombreComun) -or ($null -eq $UsuariosGitHub)){
-    $NombreComun = Read-Host "Nombre común de los proyectos"
-    $UsuarioGitHub = Read-Host "Nombre de Usuario de GitHub"
-    $DirectorioDondeSeCrearanLosProyectos = Read-Host "Directorio con la ruta completa donde se crearán los proyectos"
-    cd $DirectorioDondeSeCrearanLosProyectos
-}
+$Env:NombreComun = Read-Host "Nombre común de los proyectos"
+$Env:UsuarioGitHub = Read-Host "Nombre de Usuario de GitHub"
+$Env:DirectorioDondeSeCrearanLosProyectos = Read-Host "Directorio con la ruta completa donde se crearán los proyectos"
+cd $Env:DirectorioDondeSeCrearanLosProyectos
 
-$NombreCliente=$NombreComun + "BlazorWasm"
-$NombreApi=$NombreComun + "Api"
-$NombreModel=$NombreComun + "Model"
+$Env:NombreCliente=$NombreComun + "BlazorWasm"
+$Env:NombreApi=$NombreComun + "Api"
+$Env:NombreModel=$NombreComun + "Model"
 
-gh repo create $NombreCliente --private
-gh repo create $NombreApi --private
-gh repo create $NombreModel --private
+gh repo create $Env:NombreCliente --private
+gh repo create $Env:NombreApi --private
+gh repo create $Env:NombreModel --private
 
-git clone https://github.com/$UsuarioGitHub/$NombreCliente.git
-git clone https://github.com/$UsuarioGitHub/$NombreApi.git
-git clone https://github.com/$UsuarioGitHub/$NombreModel.git
+git clone https://github.com/$Env:UsuarioGitHub/$Env:NombreCliente.git
+git clone https://github.com/$Env:UsuarioGitHub/$Env:NombreApi.git
+git clone https://github.com/$Env:UsuarioGitHub/$Env:NombreModel.git
 
 #Model
-cd $NombreModel
+cd $Env:NombreModel
 dotnet new sln
 dotnet new gitignore
 dotnet new classlib
@@ -33,30 +29,30 @@ git commit -m"Commit inicial"
 git push
 cd ..
 #Cliente blazorwasm
-cd $NombreCliente
+cd $Env:NombreCliente
 dotnet new sln
 dotnet new gitignore
-dotnet new blazorwasm -o $NombreCliente
-dotnet sln add $NombreCliente
-git submodule add https://github.com/$UsuarioGitHub/$NombreModel.git $NombreModel
-dotnet sln add $NombreModel
+dotnet new blazorwasm -o $Env:NombreCliente
+dotnet sln add $Env:NombreCliente
+git submodule add https://github.com/$Env:UsuarioGitHub/$Env:NombreModel.git $Env:NombreModel
+dotnet sln add $Env:NombreModel
 git add .
 git commit -m"Commit inicial"
 git push
 cd ..
 
 #Api con functions
-cd $NombreApi
+cd $Env:NombreApi
 dotnet new sln
 dotnet new gitignore
-md $NombreApi
-cd $NombreApi
+md $Env:NombreApi
+cd $Env:NombreApi
 func init --worker-runtime dotnet-isolated --language c#-isolated
 func function new --name GetWeather --authlevel anonymous --template HttpTrigger
 cd ..
-dotnet sln add $NombreApi
-git submodule add https://github.com/$UsuarioGitHub/$NombreModel.git $NombreModel
-dotnet sln add $NombreModel
+dotnet sln add $Env:NombreApi
+git submodule add https://github.com/$Env:UsuarioGitHub/$Env:NombreModel.git $Env:NombreModel
+dotnet sln add $Env:NombreModel
 git add .
 git commit -m"Commit inicial"
 git push
