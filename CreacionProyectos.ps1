5f4112a26caa39d0bdc0cfd509c824eff14b98c3
Gabriel-Iban <gabriel.iban.cf@gmail.com>
2024-06-04 12:25:27 +0200

    Creación con dos repositorios y copia de model de uno a otro

--- CreacionProyectos.ps1
+++ CreacionProyectos.ps1
@@ -2,62 +2,65 @@
 
 $Env:NombreComun = Read-Host "Nombre común de los proyectos"
 $Env:UsuarioGitHub = Read-Host "Nombre de Usuario de GitHub"
-$Env:DirectorioDondeSeCrearanLosProyectos = Read-Host "Directorio con la ruta completa donde se crearán los proyectos"
-Set-Location $Env:DirectorioDondeSeCrearanLosProyectos
+$Env:DirectorioLocal = Read-Host "Directorio con la ruta completa donde se crearán los proyectos"
+Set-Location $Env:DirectorioLocal
 
 $Env:NombreCliente=$Env:NombreComun + "BlazorWasm"
-$Env:NombreApi=$Env:NombreComun + "Api"
+$Env:NombreApi=$Env:NombreComun + "Functions"
 $Env:NombreModel=$Env:NombreComun + "Model"
 
 gh repo create $Env:NombreCliente --private
 gh repo create $Env:NombreApi --private
-gh repo create $Env:NombreModel --private
 
 git clone https://github.com/$Env:UsuarioGitHub/$Env:NombreCliente.git
 git clone https://github.com/$Env:UsuarioGitHub/$Env:NombreApi.git
-git clone https://github.com/$Env:UsuarioGitHub/$Env:NombreModel.git
-
-#Model
-Set-Location $Env:NombreModel
-dotnet new sln
-dotnet new gitignore
-dotnet new classlib
-dotnet new class --name Weather
-dotnet sln add .
-git add .
-git commit -m"Commit inicial"
-git push
-Set-Location ..
-
-#Cliente blazorwasm
-Set-Location $Env:NombreCliente
-dotnet new sln
-dotnet new gitignore
-dotnet new blazorwasm -o "Client"
-dotnet sln add $Env:NombreCliente
-
-mkdir $Env:NombreModel
-Copy-Item ..\$Env:NombreModel\* .\$Env:NombreModel -Recurse
-#git submodule add https://github.com/$Env:UsuarioGitHub/$Env:NombreModel.git $Env:NombreModel
-
-dotnet sln add $Env:NombreModel
-git add .
-git commit -m"Commit inicial"
-git push
-Set-Location ..
 
 #Api con functions
 Set-Location $Env:NombreApi
 dotnet new sln
 dotnet new gitignore
+
+mkdir $Env:NombreModel
+Set-Location $Env:NombreModel
+dotnet new classlib
+dotnet new class --name Weather
+Set-Location ..
+
 mkdir $Env:NombreApi
 Set-Location $Env:NombreApi
 func init --worker-runtime dotnet-isolated --language c#-isolated
 func function new --name GetWeather --authlevel anonymous --template HttpTrigger
+dotnet add reference "..\$env:NombreModel"
 Set-Location ..
+
 dotnet sln add $Env:NombreApi
-git submodule add https://github.com/$Env:UsuarioGitHub/$Env:NombreModel.git $Env:NombreModel
 dotnet sln add $Env:NombreModel
+
+git add .
+git commit -m"Commit inicial"
+git push
+Set-Location ..
+
+
+#Cliente blazorwasm
+Set-Location $Env:NombreCliente
+dotnet new sln
+dotnet new gitignore
+
+#Copiamos el proyecto Model. Es una chapuza pero el gitsubmodule no funciona al subir a Azure
+mkdir $Env:NombreModel
+Copy-Item ..\$Env:NombreApi\$Env:NombreModel\* .\$Env:NombreModel -Recurse
+$GetModelContent = "Copy-Item ..\$Env:NombreApi\$Env:NombreModel\* .\$Env:NombreModel -Recurse -Force"
+Set-Content -Path "GetModel.ps1" -Value $GetModelContent
+
+dotnet new blazorwasm -o "Client" # El nombre Client no es casual. Es para luego subir a Azure
+Set-Location "Client"
+dotnet add reference ..\$env:NombreModel
+Set-Location ..
+
+dotnet sln add "Client"
+dotnet sln add $Env:NombreModel
+
 git add .
 git commit -m"Commit inicial"
 git push
